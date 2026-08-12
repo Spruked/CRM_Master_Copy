6ae48f9658bfd9867e8aaebf8e8f8e4d3d14d599
Bryan Anthony Spruk <bryanspruk7@outlook.com>
2026-08-11 19:28:17 -0500

    wire reversible identity operations

--- source/cali_skg/api/app.py
+++ source/cali_skg/api/app.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from cali_skg.api.cali_routes import _crm_port, app
 from cali_skg.api.contact_io_routes import router as contact_io_router
+from cali_skg.api.identity_operations_routes import router as identity_operations_router
 from cali_skg.api.operations_routes import router as operations_router
 from cali_skg.api.relationship_routes import router as relationship_intelligence_router
 
@@ -10,6 +11,7 @@ from cali_skg.api.relationship_routes import router as relationship_intelligence
 app.include_router(relationship_intelligence_router)
 app.include_router(contact_io_router)
 app.include_router(operations_router)
+app.include_router(identity_operations_router)
 
 
 if __name__ == "__main__":
